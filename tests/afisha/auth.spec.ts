84f717608214d143dd422c9d9210457590131e0a
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-10-08 16:23:33 +1000

    Change user in auth test

--- tests/afisha/auth.spec.ts
+++ tests/afisha/auth.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('should log in successfully', async ({ page }) => {
   await page.goto('', { waitUntil: 'domcontentloaded' });
+  const userId = '14497131';
 
   // на странице два элемента, которые подходят под locator('#body').getByRole('link', { name: 'Войти' }), поэтому выбираем по классам
   await page.locator('.nav_personal__btn .nav_personal__text').click();
@@ -10,7 +11,7 @@ test('should log in successfully', async ({ page }) => {
   const passwordInput = page.getByRole('textbox', { name: 'Пароль' });
   const loginButton = page.getByRole('button', { name: 'Войти с паролем' });
   const secondaryLoginButton = page.locator('.nav_personal__auth-button');
-  const userProfileLink = page.getByRole('link', { name: 'Ярослав Санников' });
+  const userProfileLink = page.getByRole('link', { name: userId });
 
   if (await secondaryLoginButton.isVisible()) {
       await secondaryLoginButton.click();
