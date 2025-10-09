4cc985d166e25e7e212a891dbfac38db65519f62
Iaroslav Sannikov <sannikov_yao@farpost.com>
2025-10-09 11:27:03 +1000

    Add search box test

---
+++ tests/afisha/search.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('should find event from search box', async ({ page }) => {
+  await page.goto('', { waitUntil: 'domcontentloaded' });
+
+  const titleElement= page.locator('.event-list__item-title > span').first();
+  await titleElement.waitFor({ state: 'visible' });
+
+  const rawTitle = await titleElement.textContent();
+  const eventTitle = rawTitle?.trim() ?? '';
+  expect(eventTitle).toBeTruthy();
+
+  const searchInput = await page.getByRole('textbox', { name: 'Поиск' });
+
+  await searchInput.fill(eventTitle);
+  await searchInput.press('Enter');
+
+  await expect(titleElement).toBeVisible();
+});
