7812017c79e070682eb366b4f39c3fa57d7b51ba
sunnycow <sannikov_yao@farpost.com>
2025-10-13 22:21:53 +1000

    Add PageFactory

---
+++ src/pages/PageFactory.ts
@@ -0,0 +1,13 @@
+import { type Page } from '@playwright/test';
+import { AfishaMainPage } from './afisha/AfishaMainPage';
+import { FarpostLoginPage } from './FarpostLoginPage';
+
+export class PageFactory {
+  static afishaMainPage(page: Page, userId: string): AfishaMainPage {
+    return new AfishaMainPage(page, userId);
+  }
+
+  static farpostLoginPage(page: Page): FarpostLoginPage {
+    return new FarpostLoginPage(page);
+  }
+}
