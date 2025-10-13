38f1be47435d4d8d2843015c1afa58e25db2bcf1
sunnycow <sannikov_yao@farpost.com>
2025-10-13 22:31:07 +1000

    Rewrite auth test to use pom

--- tests/afisha/auth.spec.ts
+++ tests/afisha/auth.spec.ts
@@ -1,26 +1,26 @@
 import { test, expect } from '@playwright/test';
+import { PageFactory } from '../../src/pages/PageFactory';
 
-test('log in', async ({ page }) => {
-  await page.goto('', { waitUntil: 'domcontentloaded' });
-  const userId = '14497131';
+test('should log in', async ({ page }) => {
+  const username = process.env.TEST_USER!;
+  const password = process.env.TEST_PASS!;
+  const userId = process.env.TEST_USER_ID!;
 
-  // на странице два элемента, которые подходят под locator('#body').getByRole('link', { name: 'Войти' }), поэтому выбираем по классам
-  await page.locator('.nav_personal__btn .nav_personal__text').click();
+  const afishaMainPage = PageFactory.afishaMainPage(page, userId);
+  const farpostLoginPage = PageFactory.farpostLoginPage(page);
 
-  const phoneInput = page.getByRole('textbox', { name: 'Телефон / Логин' });
-  const passwordInput = page.getByRole('textbox', { name: 'Пароль' });
-  const loginButton = page.getByRole('button', { name: 'Войти с паролем' });
-  const secondaryLoginButton = page.locator('.nav_personal__auth-button');
-  const userProfileLink = page.getByRole('link', { name: userId });
+  await afishaMainPage.open();
 
-  if (await secondaryLoginButton.isVisible()) {
-      await secondaryLoginButton.click();
+  await afishaMainPage.loginButton.click();
+
+  // для некоторых юзеров при нажатии на кнопку логина открывается меню
+  if (await afishaMainPage.secondaryLoginButton.isVisible({ timeout: 3000 })) {
+    await afishaMainPage.secondaryLoginButton.click();
   }
 
-  await expect(phoneInput).toBeVisible()
-  await phoneInput.fill(process.env.TEST_USER!);
-  await passwordInput.fill(process.env.TEST_PASS!);
+  await farpostLoginPage.phoneInput.fill(username);
+  await farpostLoginPage.passwordInput.fill(password);
 
-  await loginButton.click();
-  await expect(userProfileLink).toBeVisible();
+  await farpostLoginPage.passwordInput.press('Enter');
+  await expect(afishaMainPage.userProfileLink).toBeVisible();
 });
