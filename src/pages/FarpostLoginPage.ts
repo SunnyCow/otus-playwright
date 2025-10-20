8b196a15f46f811d91da5b95d87095aa44fec848
sunnycow <sannikov_yao@farpost.com>
2025-10-20 18:17:18 +1000

    Add login method to FarpostLoginPage

--- src/pages/FarpostLoginPage.ts
+++ src/pages/FarpostLoginPage.ts
@@ -8,4 +8,10 @@ export class FarpostLoginPage {
     this.phoneInput = page.getByRole('textbox', { name: 'Телефон / Логин' });
     this.passwordInput = page.getByRole('textbox', { name: 'Пароль' });
   }
+
+  async login(username: string, password: string): Promise<void> {
+    await this.phoneInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.passwordInput.press('Enter');
+  }
 }
