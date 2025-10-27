57c6c74fd8646c81b890cd622dd2f8d091c63ddc
sunnycow <sannikov_yao@farpost.com>
2025-10-27 14:04:09 +1000

    Add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,22 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import playwright from 'eslint-plugin-playwright';
 
 export default tseslint.config(
   {
     ignores: [
-      "node_modules/",
-      "playwright-report/",
-      "test-results/",
-      "playwright.config.ts",
-      "eslint.config.mjs",
+      'node_modules',
+      'playwright-report',
+      'test-results',
+      'eslint.config.*',
+      'playwright.config.*',
     ],
   },
 
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
+  ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
@@ -24,11 +25,29 @@ export default tseslint.config(
       },
     },
     rules: {
-      "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/no-misused-promises": "error",
-      "@typescript-eslint/await-thenable": "error",
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-      "no-console": "off",
+      'no-console': 'error',
+      'prefer-const': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/no-empty-function': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
-  }
+  },
+
+  {
+    files: ['tests/**/*.ts'],
+    ...playwright.configs['flat/recommended'],
+    rules: {
+      'playwright/no-page-pause': 'warn',
+      '@typescript-eslint/no-empty-function': 'off',
+      'playwright/missing-playwright-await': 'error',
+      'playwright/no-useless-await': 'error',
+      'playwright/no-skipped-test': 'error',
+    },
+  },
 );
