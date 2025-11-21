6ebb0e126b310f7bc7cd68cf350a09dd284e4b50
sunnycow <sannikov_yao@farpost.com>
2025-11-21 15:47:56 +1000

    Update event-creation-page test

--- tests/afisha/event-creation.spec.ts
+++ tests/afisha/event-creation.spec.ts
@@ -2,20 +2,20 @@ import { test, expect } from '../../fixtures/auth.fixture';
 import { PageFactory } from '../../src/pages/PageFactory';
 
 test.describe('User Event Creation Page', () => {
-    test.describe('Smoke UI Verification @smoke', () => {
-        test('loads the page and displays core elements', async ({ userPage }) => {
-            const eventPage = PageFactory.eventCreationPage(userPage);
-            await eventPage.open();
+  test.describe('Smoke UI Verification @smoke', () => {
+    test('loads the page and displays core elements', async ({ userPage }) => {
+      const eventPage = PageFactory.eventCreationPage(userPage);
+      await eventPage.open();
 
-            await expect(eventPage.titleCard).toBeVisible({ timeout: 20_000 });
-            await expect(eventPage.posterCard).toBeVisible();
-            await expect(eventPage.descriptionCard).toBeVisible();
-            await expect(eventPage.contactsCard).toBeVisible();
-            await expect(eventPage.priceCard).toBeVisible();
-            await expect(eventPage.schedulesCard).toBeVisible();
-            await expect(eventPage.orgContactsCard).toBeVisible();
-            await expect(eventPage.registrationCard).toBeVisible();
-            await expect(eventPage.publishButton).toBeDisabled();
-        });
+      await expect(eventPage.basicInfoComponent.root).toBeVisible({ timeout: 20_000 });
+      await expect(eventPage.posterComponent.root).toBeVisible();
+      await expect(eventPage.descriptionComponent.root).toBeVisible();
+      await expect(eventPage.eventContactsComponent.root).toBeVisible();
+      await expect(eventPage.priceComponent.root).toBeVisible();
+      await expect(eventPage.schedulesComponent.root).toBeVisible();
+      await expect(eventPage.registrationComponent.root).toBeVisible();
+      await expect(eventPage.orgContactsComponent.root).toBeVisible();
+      await expect(eventPage.publishButton).toBeDisabled();
     });
+  });
 });
