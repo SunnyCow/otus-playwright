067c34130e21acaa1b1a25afb8b5d8163f033a3e
sunnycow <sannikov_yao@farpost.com>
2025-11-21 15:07:28 +1000

    Refactor component files
    - Переименовал папку fragments в components
    - Переименовал Section в Component
    - Добавил наследование от BaseComponent во все классы компонентов

---
+++ src/pages/components/afisha/user/event-creation/DescriptionComponent.ts
@@ -0,0 +1,5 @@
+import { BaseComponent } from '../../../../../base/BaseComponent';
+
+export class DescriptionComponent extends BaseComponent {
+  public readonly editor = this.root.frameLocator('.tox-edit-area__iframe').locator('#tinymce');
+}
