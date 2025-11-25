2a8ffcc05501675b76f2901489d187b30a57acaa
sunnycow <sannikov_yao@farpost.com>
2025-11-25 16:27:04 +1000

    Add fillDescription to DescriptionComponent

--- src/pages/afisha/user/event-creation/DescriptionComponent.ts
+++ src/pages/afisha/user/event-creation/DescriptionComponent.ts
@@ -2,4 +2,9 @@ import { BaseComponent } from '../../../../base/BaseComponent';
 
 export class DescriptionComponent extends BaseComponent {
   public readonly editor = this.root.frameLocator('.tox-edit-area__iframe').locator('#tinymce');
+
+  public async fillDescription(text = 'Lorem ipsum'): Promise<void> {
+    await this.editor.click();
+    await this.editor.fill(text);
+  }
 }
