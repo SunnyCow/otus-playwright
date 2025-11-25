8bc02cb0ff6a3b4e39e25098efea5461df7c62b6
sunnycow <sannikov_yao@farpost.com>
2025-11-25 16:27:53 +1000

    Add selectMinAge to MinAgeComponent

--- src/pages/afisha/user/event-creation/MinAgeComponent.ts
+++ src/pages/afisha/user/event-creation/MinAgeComponent.ts
@@ -1,5 +1,14 @@
 import { BaseComponent } from '../../../../base/BaseComponent';
 
+export type MinAge = '0+' | '6+' | '12+' | '16+' | '18+';
+
 export class MinAgeComponent extends BaseComponent {
   public readonly minAgeSelect = this.root.getByLabel(/Возрастное ограничение/);
+  public readonly minAgeList = this.getPage().getByRole('listbox', { name: /Возрастное ограничение/ });
+  public readonly minAgeListOptions = this.minAgeList.getByRole('option');
+
+  public async selectMinAge(ageValue: MinAge): Promise<void> {
+    await this.minAgeSelect.click();
+    await this.minAgeListOptions.filter({ hasText: ageValue }).click();
+  }
 }
