4d37428d18906bb4ee35616e0ba6be152c8b1d76
sunnycow <sannikov_yao@farpost.com>
2025-11-25 11:58:30 +1000

    Update imports and fix small things

--- src/pages/afisha/AfishaMainPage.ts
+++ src/pages/afisha/AfishaMainPage.ts
@@ -2,7 +2,7 @@ import { BasePage } from '../../base/BasePage';
 import { AfishaHeaderComponent } from './components/AfishaHeaderComponent';
 
 export class AfishaMainPage extends BasePage {
-  public readonly header = new AfishaHeaderComponent(this.page.locator('.header'));
+  public readonly header = new AfishaHeaderComponent(this.page.locator('.header'), this.page);
   public readonly eventTitle = this.page.locator('.event-list__item-title > span');
   public readonly eventFavButton = this.page.locator('.event-list__item-link-favorite').first();
   public readonly carouselEvent = this.page.locator('.event-list__item-image').first();
--- src/pages/kino/KinoMainPage.ts
+++ src/pages/kino/KinoMainPage.ts
@@ -2,5 +2,5 @@ import { BasePage } from '../../base/BasePage';
 import { KinoHeaderComponent } from './components/KinoHeaderComponent';
 
 export class KinoMainPage extends BasePage {
-  public readonly header = new KinoHeaderComponent(this.page.locator('.header'));
+  public readonly header = new KinoHeaderComponent(this.page.locator('.header'), this.page);
 }
