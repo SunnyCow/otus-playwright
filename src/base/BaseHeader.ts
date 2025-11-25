8ad5e0d553d2412c57a6a1cf98f1919c33b3160e
sunnycow <sannikov_yao@farpost.com>
2025-11-25 11:58:06 +1000

    Refactor BaseHeader

--- src/base/BaseHeader.ts
+++ src/base/BaseHeader.ts
@@ -1,18 +1,17 @@
+// Пока оставлю этот класс, ещё подумаю над архитектурой
+
 import { type Locator, type Page } from '@playwright/test';
+import { BaseComponent } from './BaseComponent';
 
-export class BaseHeader {
-  readonly logo: Locator;
-  readonly projectCity: Locator;
-  readonly dropdown: Locator;
-  readonly dropdownCity: Locator;
-  readonly searchInput: Locator;
+export class BaseHeader extends BaseComponent {
+  public readonly logo = this.root.getByRole('link', { name: 'VL.ru', exact: true });
+  public readonly projectCity = this.root.locator('.header__project-city').nth(1);
+  public readonly dropdown = this.root.locator('.dropdown-menu').nth(1);
+  public readonly dropdownCity = this.dropdown.locator(':scope > a').first();
+  public readonly searchInput = this.root.getByRole('textbox', { name: 'Поиск' });
 
-  constructor(readonly page: Page) {
-    this.logo = page.getByRole('link', { name: 'VL.ru', exact: true });
-    this.projectCity = page.locator('.header__project-city').nth(1);
-    this.dropdown = page.locator('.dropdown-menu').nth(1);
-    this.dropdownCity = this.dropdown.locator(':scope > a').first();
-    this.searchInput = page.getByRole('textbox', { name: 'Поиск' });
+  constructor(root: Locator, page: Page) {
+    super(root, page);
   }
 
   async openCityDropdown(): Promise<void> {
