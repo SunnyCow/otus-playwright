63fea2934226e70afff453c1eb18f7b5950af077
sunnycow <sannikov_yao@farpost.com>
2025-11-28 12:10:28 +1000

    Add fillEmail method in OrgContactsComponent

--- src/pages/afisha/user/event-creation/OrgContactsComponent.ts
+++ src/pages/afisha/user/event-creation/OrgContactsComponent.ts
@@ -3,4 +3,9 @@ import { BaseComponent } from '../../../../base/BaseComponent';
 export class OrgContactsComponent extends BaseComponent {
   public readonly emailInput = this.root.getByLabel('Email');
   public readonly contactsInput = this.root.getByLabel('Дополнительные контакты');
+
+  public async fillEmail(email = 'sannikov_yao@farpost.com'): Promise<void> {
+    await this.emailInput.click();
+    await this.emailInput.fill(email);
+  }
 }
