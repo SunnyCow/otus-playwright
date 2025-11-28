d44fa6ddafb780c95f24875730b367c14b0bc0fc
sunnycow <sannikov_yao@farpost.com>
2025-11-28 12:11:05 +1000

    Add selectVenue method in SchedulesComponent

--- src/pages/afisha/user/event-creation/SchedulesComponent.ts
+++ src/pages/afisha/user/event-creation/SchedulesComponent.ts
@@ -2,8 +2,16 @@ import { BaseComponent } from '../../../../base/BaseComponent';
 
 export class SchedulesComponent extends BaseComponent {
   public readonly venueInput = this.root.getByLabel('Место проведения');
+  public readonly venueList = this.getPage().getByRole('listbox');
+  public readonly venueListOptions = this.venueList.getByRole('option');
   public readonly addressInput = this.root.getByLabel('Адрес');
   public readonly dateInput = this.root.getByLabel('Дата');
   public readonly timeSelect = this.root.getByLabel('Время');
   public readonly addMoreSchedulesButton = this.root.getByRole('button', { name: 'Добавить еще' });
+
+  public async selectVenue(venue = 'Daisy'): Promise<void> {
+    await this.venueInput.click();
+    await this.venueInput.fill(venue);
+    await this.venueListOptions.filter({ hasText: 'Daisy' }).click();
+  }
 }
