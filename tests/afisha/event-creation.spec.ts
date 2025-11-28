235495ac08e8eca136f03e4d46d11c2159f0fd30
sunnycow <sannikov_yao@farpost.com>
2025-11-28 12:13:46 +1000

    Add publish test to event-creation.spec.ts

--- tests/afisha/event-creation.spec.ts
+++ tests/afisha/event-creation.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '../../fixtures/auth.fixture';
+import { EventCreationPage } from '../../src/pages/afisha/user/event-creation/EventCreationPage';
 import { PageFactory } from '../../src/pages/PageFactory';
 
 test.describe('User Event Creation Page', () => {
-  test.describe('Smoke UI Verification @smoke', () => {
-    test('loads the page and displays core elements', async ({ userPage }) => {
-      const eventPage = PageFactory.eventCreationPage(userPage);
-      await eventPage.open();
+  let eventPage: EventCreationPage;
 
+  test.beforeEach(async ({ userPage }) => {
+    eventPage = PageFactory.eventCreationPage(userPage);
+    await eventPage.open();
+  })
+
+  test.describe('Smoke UI Verification', { tag: '@smoke' }, () => {
+    test('loads the page and displays core elements', async () => {
       await expect(eventPage.basicInfoComponent.root).toBeVisible({ timeout: 20_000 });
       await expect(eventPage.posterComponent.root).toBeVisible();
       await expect(eventPage.descriptionComponent.root).toBeVisible();
@@ -18,4 +23,20 @@ test.describe('User Event Creation Page', () => {
       await expect(eventPage.publishButton).toBeDisabled();
     });
   });
+
+  test.describe('Event creation', () => {
+    test('publish button is enabled if all required fields are filled', async () => {
+      await eventPage.fillAllRequiredFields();
+
+      await expect(eventPage.publishButton).toBeEnabled();
+    });
+
+    test('creates event with all required fields filled', async () => {
+      test.skip(test.info().project.name.toLowerCase().includes('prod'), 'Staging-only test');
+
+      await eventPage.fillAllRequiredFields();
+
+      await eventPage.publishButton.click();
+    });
+  })
 });
